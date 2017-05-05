7a33ee2e40b19276cc28015ec78e33fa6f9de63c
Henry He <henry.he44@qmail.cuny.edu>
2017-05-05 03:24:53 -0400

    Added basic routes in express.js

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+	res.send('Success!');
+});
+
+app.get('/home', function(req,res){
+	res.send('home!');
+});
+
+app.get('/wines', function(req,res){
+	res.send('wines!');
+});
+
+app.get('/spirits', function(req,res){
+	res.send('spirits!');
+});
+
+app.get('/policies', function(req,res){
+	res.send('policies!');
+});
+
+app.get('*', function(req,res){
+	res.send('badroute!');
+});
+
+var server = app.listen(3000, function(){
+	console.log('Listening on port 3000');
+});
